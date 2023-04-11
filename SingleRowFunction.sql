8fcde3de020066d4e912ed53709d7cde571c3cb3
Savanna Piscitelli <savannapiscitelli@gmail.com>
2023-04-10 20:57:48 -0600

    Complete q2 apart from dollar sign conversion

--- SingleRowFunction.sql
+++ SingleRowFunction.sql
@@ -1,10 +1,10 @@
-rem Lab Unit 10-11 Simple SELECT and Sorting 
+-- Lab Unit 10-11 Simple SELECT and Sorting 
 set echo on
 set linesize 120
 set pagesize 66
 spool C:\cprg250s\Single_Row_Function_Lab\unit13LabOutput.txt
 
-rem Q1 - basic select columns with headers
+-- Q1 - basic select columns with headers
 
 column "Desc" format a20
 column "Start Date" format a20
@@ -21,21 +21,25 @@ order by 1,4,5;
 clear breaks
 clear columns
 
-rem Q2 
+-- Q2 - All destination tours outside of Canada and United States, price converted to a number of dollar signs
 
-rem insert solution here!
+column "Desc" format a40
+column "Country" format a20
+column "Cost" format a10
+column "City" format a20
+select dest_description  "Desc", 
+   rpad('$', price / 100 , '$') "Cost", 
+   upper(country) "Country", city "City"
+from rcv_destination
+where country not in 'Canada' and country not in 'United States'
+order by 3, 4, 2;
+clear columns
 
-rem Q3 
+-- Q3 
 
-rem insert solution here!
+-- insert solution here!
 
-rem Q4 
 
-rem insert solution here!
-
-rem Q5 
-
-rem insert solution here!
 
 spool off
 
